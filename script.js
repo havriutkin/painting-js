809e01c5d978f138227b608018dca9c5318ae13b
havriutkin <havriutkin@gmail.com>
2023-07-24 17:40:49 -0400

    Add color picker

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // Get elements from a document
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const clearButton = document.getElementById('clear');
+const instrumentTypeInput = document.getElementById('type');
 const sizeInput = document.getElementById('size');
-const instumentTypeInput = document.getElementById('type');
-
-
-// Global variables
-let isActive = false;
+const colorPicker = document.getElementById('colorPicker')
+const clearButton = document.getElementById('clear');
 
 // Set the canvas size explicitly in JavaScript
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
 
+// Objects
+// Mouse object
 const mouse = {
     x: undefined,
     y: undefined,
@@ -21,16 +20,19 @@ const mouse = {
     lastY: undefined,
 }
 
-const instument = {
+// Instrument object
+const instrument = {
     type: 'pencil',
-    size: 1,
+    size: 5,
     color: 'white',
+    isActive: false,
 }
 
+
 // Event handlers
-canvas.addEventListener('mousedown', () => isActive = true);
-canvas.addEventListener('mouseup', () => isActive = false);
-canvas.addEventListener('mouseout', () => isActive = false);
+canvas.addEventListener('mousedown', () => instrument.isActive = true);
+canvas.addEventListener('mouseup', () => instrument.isActive = false);
+canvas.addEventListener('mouseout', () => instrument.isActive = false);
 canvas.addEventListener('mousemove', (event) => {
     mouse.lastX = mouse.x;
     mouse.lastY = mouse.y;
@@ -43,11 +45,15 @@ clearButton.addEventListener('click', () => {
 });
 
 sizeInput.addEventListener('input', (event) => {
-    instument.size = event.target.value;
+    instrument.size = event.target.value;
 });
 
-instumentTypeInput.addEventListener('input', (event) => {
-    instument.type = event.target.value;
+instrumentTypeInput.addEventListener('input', (event) => {
+    instrument.type = event.target.value;
+});
+
+colorPicker.addEventListener('change', (event) => {
+    instrument.color = event.target.value;
 });
 
 
@@ -60,6 +66,7 @@ function drawCircle(x, y, radius, color){
     ctx.fill();
 }
 
+// Function draws a line along mouse trajectory
 function draw(color){
     const distance = Math.sqrt((mouse.x - mouse.lastX) ** 2 + (mouse.y - mouse.lastY) ** 2);
         const smoothness = 2;
@@ -68,24 +75,32 @@ function draw(color){
     for (let i = 0; i < distance; i += smoothness) {
         const x = mouse.lastX + (mouse.x - mouse.lastX) * (i / distance);
         const y = mouse.lastY + (mouse.y - mouse.lastY) * (i / distance);
-        drawCircle(x, y, instument.size, color);
+        drawCircle(x, y, instrument.size, color);
     }
 }
 
+// Function draws a line with the same color as background along mouse trajectory 
 function erase(){
     draw('black');
 }
 
+
 // Main loop
 function sceneLoop(){
-    if (isActive){
-        if (instument.type === 'pencil')
-            draw(instument.color);
-        else if (instument.type === 'eraser')
+    if (instrument.isActive){
+        console.log(instrument.type);
+        if (instrument.type === 'pencil'){
+            draw(instrument.color);
+            console.log('i am pencil');
+        }
+        else if (instrument.type === 'eraser'){
+            console.log('i am eraser');
             erase();
+        }
     }
 
     requestAnimationFrame(sceneLoop);
 }
 
+// Call main loop function
 sceneLoop();
